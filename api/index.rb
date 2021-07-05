81d51611be7fee6a87632f8a2fdbbc515644dfca
Lucas Azzola <lucas@azzola.dev>
2021-07-05 21:40:24 +1000

    .

---
+++ api/index.rb
@@ -0,0 +1,7 @@
+require 'strava-ruby-client'
+client = Strava::Api::Client.new access_token: ENV.fetch('STRAVA_ACCESS_TOKEN')
+Handler = Proc.new do |req, res|
+  res.status = 200
+  res['content-type'] = 'text/html'
+  res.body = client.gear('b7641128').distance_s
+end
