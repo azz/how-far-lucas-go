3428c66ec4846d093f1baf109298edecd6d3a9a0
Arran Ubels <arran4@gmail.com>
2021-07-06 22:10:00 +1000

    content-type text (#1)

--- api/index.rb
+++ api/index.rb
@@ -2,6 +2,6 @@ require 'strava-ruby-client'
 client = Strava::Api::Client.new access_token: ENV.fetch('STRAVA_ACCESS_TOKEN')
 Handler = Proc.new do |req, res|
   res.status = 200
-  res['content-type'] = 'text/html'
+  res['content-type'] = 'text/plain'
   res.body = client.gear('b7641128').distance_s
 end
